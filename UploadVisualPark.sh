93dbb1e95527ea2dd1eba68673921931036e6886
Samantha Deshazer <30559989+sdeshazer@users.noreply.github.com>
2018-06-24 02:46:35 -0700

    Update UploadVisualPark.sh

--- UploadVisualPark.sh
+++ UploadVisualPark.sh
@@ -1,12 +1,9 @@
 while true
 do
-host=rasbperrypi #if needed
-user=username #if needed 
-pass=password #if needed
 cd /home/pi/
-python capture.py #captures image resolution of (1280 x 720)
+python capture.py
+sleep 2
 cd /home/pi/projects/VisualPark/
 ./VisualPark
-curl --user lcccodin:password -T /home/pi/projects/VisualPark/Output/ParkingStatuses.yml ftp://www.lcccodingclub.org/public_html/parkingapp/ParkingStatuses.yml
-sleep 30
+curl  --user lcccodin:3C73m3vjqA -T /home/pi/projects/VisualPark/Output/ParkingStatuses.yml  ftp://www.lcccodingclub.org/public_html/ParkingStatuses.yml
 done
