61e5770f5924dcd424a555ba6fec8c26ec711a0c
Samantha Deshazer <30559989+sdeshazer@users.noreply.github.com>
2018-06-24 02:44:42 -0700

    Update Capture.py

--- Capture.py
+++ Capture.py
@@ -2,8 +2,7 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-camera.resolution = (1280, 720)
-camera.start_preview()
-# Camera warm-up time
-time.sleep(2)
-camera.capture('image.jpg')
+    camera.resolution = (1280, 720)
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture('/home/pi/projects/VisualPark/images/latest.png')
